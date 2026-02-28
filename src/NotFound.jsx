ae5040f26247e42cdec24c4334540a7e855279b1
minhmatrix <125466698+minhmatrix@users.noreply.github.com>
2026-02-28 20:55:07 +0700

    Create NotFound.jsx

---
+++ src/NotFound.jsx
@@ -0,0 +1,8 @@
+// src/NotFound.jsx
+export default function NotFound() {
+  // Trả về null sẽ làm màn hình trắng tinh
+  return null; 
+  
+  // Nếu bạn muốn hiện chữ thì dùng dòng này thay thế:
+  // return <div style={{ textAlign: "center", marginTop: "50px" }}>404 - Not Found</div>;
+}
