8aa442fc2aa3e8834fc4fb172c59dd9d218ee655
minhmatrix <125466698+minhmatrix@users.noreply.github.com>
2026-02-28 15:26:50 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,25 @@
+// src/main.jsx
 import "./index.css";
-import App from "./App.jsx";
-import Simple from "./simple.jsx";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Intro from "./Intro";
 import AuthCodeForm from "./AuthCodeForm";
-import LoginForm from "./LoginForm.jsx";
-import Live from "./Live"; // new live page component
-import Contact from "./Contact"; // contact page
+import Live from "./Live";
+import Contact from "./Contact";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
+      {/* Tuyệt đối KHÔNG thêm Route cho path="/" ở đây */}
+      
       <Route
         path="/two_step_verification/two_factor"
         element={<AuthCodeForm />}
       />
       <Route path="/live" element={<Live />} />
       <Route path="/contact/:slug" element={<Contact />} />
+
+      {/* Route này sẽ bắt tất cả các trang không khai báo (bao gồm cả trang chủ) và để trống */}
+      <Route path="*" element={<></>} /> 
     </Routes>
   </BrowserRouter>
 );
