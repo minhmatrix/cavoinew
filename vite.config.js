e840996fc6224a5bd05a30513a5bb4980d0a6305
minhmatrix <125466698+minhmatrix@users.noreply.github.com>
2026-02-28 15:01:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    // nothing special here any more
-  },
-  plugins: [
-    react(),
-    {
-      name: 'root-404',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url === '/') {
-            res.statusCode = 404;
-            res.end();
-          } else {
-            next();
-          }
-        });
-      }
+plugins: [
+  react(),
+  {
+    name: 'root-404',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === '/') {
+          res.statusCode = 404;
+          res.end();
+        } else {
+          next();
+        }
+      });
     }
-  ]
-})
+  }
+]
